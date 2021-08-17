943eda503eef077836c14ca991d02d19ee4cedd3
Efe <Efe@DESKTOP-U2CPHC9>
2021-08-17 19:01:48 -0400

    Created the modules of ShopaHoop eCommerce Spring project.

---
+++ ShopaHoopProject/ShopaHoopWebParent/ShopaHoopBackEnd/src/main/java/com/shopahoop/admin/ShopaHoopBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.shopahoop.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopaHoopBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopaHoopBackEndApplication.class, args);
+	}
+
+}
---
+++ ShopaHoopProject/ShopaHoopWebParent/ShopaHoopFrontEnd/src/main/java/com/shopahoop/site/ShopaHoopFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.shopahoop.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopaHoopFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopaHoopFrontEndApplication.class, args);
+	}
+
+}
