fb755c8331db39315b86acb05dcad111a667c93a
Efe <Efe@DESKTOP-U2CPHC9>
2021-08-19 18:17:32 -0400

    Established JDBC connection. Created Role and User Schemas and Tables.

--- ShopaHoopProject/ShopaHoopWebParent/ShopaHoopBackEnd/src/main/java/com/shopahoop/admin/ShopaHoopBackEndApplication.java
+++ ShopaHoopProject/ShopaHoopWebParent/ShopaHoopBackEnd/src/main/java/com/shopahoop/admin/ShopaHoopBackEndApplication.java
@@ -2,8 +2,10 @@ package com.shopahoop.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.shopahoop.common.entity", "com.shopahoop.admin.user"})
 public class ShopaHoopBackEndApplication {
 
 	public static void main(String[] args) {
